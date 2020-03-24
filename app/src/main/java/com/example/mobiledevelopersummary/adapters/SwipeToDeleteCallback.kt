b9cb43aa595c5a02e1df73b808a58719011b5822
Sergio Leech <plusphyto@gmail.com>
2020-03-24 11:33:34 +0200

    Changed Swipe delete from MyFolderFragment

---
+++ app/src/main/java/com/example/mobiledevelopersummary/adapters/SwipeToDeleteCallback.kt
@@ -0,0 +1,82 @@
+package com.example.mobiledevelopersummary.adapters
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mobiledevelopersummary.R
+
+open class SwipeToDeleteCallback(val context: Context) : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+    override fun onMove(
+        recyclerView: RecyclerView,
+        viewHolder: RecyclerView.ViewHolder,
+        target: RecyclerView.ViewHolder
+    ): Boolean {
+        return false
+    }
+
+    @SuppressLint("ResourceAsColor")
+    override fun onChildDraw(
+        c: Canvas,
+        recyclerView: RecyclerView,
+        viewHolder: RecyclerView.ViewHolder,
+        dX: Float,
+        dY: Float,
+        actionState: Int,
+        isCurrentlyActive: Boolean
+    ) {
+        val itemView = viewHolder.itemView
+        val deleteIcon=ContextCompat.getDrawable(context,R.drawable.delete_item)
+
+        val colorDrawableBackground=ColorDrawable(R.color.colorBlueGrey400)
+        val iconMarginVertical = (viewHolder.itemView.height - deleteIcon!!.intrinsicHeight) / 2
+
+        if (dX > 0) {
+            colorDrawableBackground.setBounds(itemView.left, itemView.top, dX.toInt(), itemView.bottom)
+            deleteIcon.setBounds(
+                itemView.left + iconMarginVertical,
+                itemView.top + iconMarginVertical,
+                itemView.left + iconMarginVertical + deleteIcon.intrinsicWidth,
+                itemView.bottom - iconMarginVertical
+            )
+        } else {
+            colorDrawableBackground.setBounds(
+                itemView.right + dX.toInt(),
+                itemView.top,
+                itemView.right,
+                itemView.bottom
+            )
+            deleteIcon.setBounds(
+                itemView.right - iconMarginVertical - deleteIcon.intrinsicWidth,
+                itemView.top + iconMarginVertical,
+                itemView.right - iconMarginVertical,
+                itemView.bottom - iconMarginVertical
+            )
+            deleteIcon.level = 0
+        }
+
+        colorDrawableBackground.draw(c)
+
+        c.save()
+
+        if (dX > 0)
+            c.clipRect(itemView.left, itemView.top, dX.toInt(), itemView.bottom)
+        else
+            c.clipRect(itemView.right + dX.toInt(), itemView.top, itemView.right, itemView.bottom)
+
+        deleteIcon.draw(c)
+
+        c.restore()
+        super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+    }
+
+
+    override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+
+    }
+
+}
--- app/src/main/java/com/example/mobiledevelopersummary/bottom_navigation/MyFolderFragment.kt
+++ app/src/main/java/com/example/mobiledevelopersummary/bottom_navigation/MyFolderFragment.kt
@@ -1,16 +1,22 @@
 package com.example.mobiledevelopersummary.bottom_navigation
 
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
+import com.example.mobiledevelopersummary.R
 import com.example.mobiledevelopersummary.adapters.MyContentListAdapter
+import com.example.mobiledevelopersummary.adapters.SwipeToDeleteCallback
 import com.example.mobiledevelopersummary.database.ContentDatabase
 import com.example.mobiledevelopersummary.databinding.FragmentMainFolderBinding
 import com.example.mobiledevelopersummary.databinding.FragmentMyFolderBinding
@@ -46,8 +52,8 @@ private lateinit var binding:FragmentMyFolderBinding
             }
         })
 
-
-        val itemTouchHelperCallback=object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT){
+          // Swipe delete item
+        /*val itemTouchHelperCallback=object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT){
             override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, target: RecyclerView.ViewHolder): Boolean {
                       return false
             }
@@ -59,9 +65,61 @@ private lateinit var binding:FragmentMyFolderBinding
 
             }
 
-        }
+            override fun onChildDraw(c: Canvas, recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, dX: Float, dY: Float, actionState: Int, isCurrentlyActive: Boolean) {
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+                val itemView = viewHolder.itemView
+                val deleteIcon=ContextCompat.getDrawable(application,R.drawable.delete)
 
-        ItemTouchHelper(itemTouchHelperCallback).attachToRecyclerView(recyclerView)
+                val colorDrawableBackground=ColorDrawable(Color.WHITE)
+                val iconMarginVertical = (viewHolder.itemView.height - deleteIcon!!.intrinsicHeight) / 2
+
+                if (dX > 0) {
+                    colorDrawableBackground.setBounds(itemView.left, itemView.top, dX.toInt(), itemView.bottom)
+                    deleteIcon.setBounds(
+                        itemView.left + iconMarginVertical,
+                        itemView.top + iconMarginVertical,
+                        itemView.left + iconMarginVertical + deleteIcon.intrinsicWidth,
+                        itemView.bottom - iconMarginVertical
+                    )
+                } else {
+                    colorDrawableBackground.setBounds(
+                        itemView.right + dX.toInt(),
+                        itemView.top,
+                        itemView.right,
+                        itemView.bottom
+                    )
+                    deleteIcon.setBounds(
+                        itemView.right - iconMarginVertical - deleteIcon.intrinsicWidth,
+                        itemView.top + iconMarginVertical,
+                        itemView.right - iconMarginVertical,
+                        itemView.bottom - iconMarginVertical
+                    )
+                    deleteIcon.level = 0
+                }
+
+                colorDrawableBackground.draw(c)
+
+                c.save()
+
+                if (dX > 0)
+                    c.clipRect(itemView.left, itemView.top, dX.toInt(), itemView.bottom)
+                else
+                    c.clipRect(itemView.right + dX.toInt(), itemView.top, itemView.right, itemView.bottom)
+
+                deleteIcon.draw(c)
+
+                c.restore()
+            }
+
+        }
+        ItemTouchHelper(itemTouchHelperCallback).attachToRecyclerView(recyclerView)*/
+        val swipeHandler = object : SwipeToDeleteCallback(application){
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                myContentListViewModel.delete(adapter.getMyContent(viewHolder.adapterPosition))
+                adapter.deletePosition(viewHolder.adapterPosition)
+            }
+        }
+        ItemTouchHelper(swipeHandler).attachToRecyclerView(recyclerView)
         return binding.root
     }
 
